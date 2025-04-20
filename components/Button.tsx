21de4ac493042691cce310520a42cd01f7921130
okdevblog <okdevblog@gmail.com>
2025-04-20 13:22:19 +0000

    2

--- components/Button.tsx
+++ components/Button.tsx
@@ -15,7 +15,8 @@ export default function Button({ label, theme, onPress }: Props) {
           styles.buttonContainer,
           { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
         ]}>
-        <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
+        {/* <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}> */}
+        <Pressable style={styles.button}  onPress={onPress} >
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
         </Pressable>
